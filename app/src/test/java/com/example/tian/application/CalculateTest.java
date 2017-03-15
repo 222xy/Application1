cc7adc3167488f273cfa8ba31bc561bd9232e146
tian xu <tian xu>
2017-03-15 10:32:35 +0800

    Initial commit

---
+++ app/src/test/java/com/example/tian/application/CalculateTest.java
@@ -0,0 +1,37 @@
+package com.example.tian.application;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tian on 2017/3/15.
+ */
+public class CalculateTest {
+private  Calculate calculate;
+    @Before
+    public void setUp() throws Exception {
+calculate=new Calculate();
+    }
+
+    @Test
+    public void testSum() throws Exception {
+        assertEquals(6d,calculate.sum(1d,5d),0);
+    }
+
+    @Test
+    public void testSubstract() throws Exception {
+        assertEquals(4d,calculate.substract(5d,1d),0);
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+        assertEquals(5d,calculate.divide(1d,5d),0);
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+        assertEquals(1d,calculate.multiply(5d,5d),0);
+    }
+}
